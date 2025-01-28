c87f40664916b5721de1bea7c24847d626fd3c97
Kai Vandivier <kpvandivier@gmail.com>
2025-01-28 17:32:16 +0100

    chore(prediction-app): add tentative app hub info

--- packages/prediction-app/d2.config.js
+++ packages/prediction-app/d2.config.js
@@ -1,6 +1,10 @@
 const config = {
     type: 'app',
     title: 'Prediction App',
+
+    id: 'a29851f9-82a7-4ecd-8b2c-58e0f220bc75',
+    minDHIS2Version: '2.41',
+
     entryPoints: {
         app: './src/App.tsx',
     },
