98c0b19634db0cbddfb1130b8eed92f9a16782b3
Kai Vandivier <kpvandivier@gmail.com>
2025-01-30 15:00:14 +0100

    chore: little hack to skip app hub workflow for Eval app and avoid breaking

---
+++ packages/chap-eval/d2.config.js
@@ -0,0 +1,10 @@
+/**
+ * This file & config is a little hack to skip this package when running the
+ * DHIS2 action-semantic-release custom action
+ */
+
+const config = {
+    type: 'lib',
+}
+
+module.exports = config
