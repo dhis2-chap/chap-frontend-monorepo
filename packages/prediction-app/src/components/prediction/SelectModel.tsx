a93e86b9d7fa6a10ff5466d63b4970b642288c41
Karim Bahgat <karimba@uio.no>
2025-03-03 12:20:01 +0100

    fix: switch to dhis2 buttons, better external link, misc cleanup

--- packages/prediction-app/src/components/prediction/SelectModel.tsx
+++ packages/prediction-app/src/components/prediction/SelectModel.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { DefaultService, Feature, ModelSpec } from '@dhis2-chap/chap-lib'
-import { SingleSelectField, SingleSelectOption } from '@dhis2/ui'
+import { SingleSelectField, SingleSelectOption, Button } from '@dhis2/ui'
 import styles from './styles/selectModel.module.css'
 import i18n from '@dhis2/d2-i18n'
 
@@ -53,66 +53,7 @@ const createTargetAsFeature = (target_name: string): Feature => {
     }
 }
 
-const SelectModelOLD = ({ selectedModel, setSelectedModel }: SelectModelProps) => {
-    const [models, setModels] = useState<ModelSpec[]>([])
-    const [isLoadingModels, setIsLoadingModels] = useState(true)
-
-    const getModels = async () => {
-        await DefaultService.listModelsListModelsGet()
-            .then((response: ModelSpec[]) => {
-                //turn target_name into a feature
-                let models: ModelSpec[] = response.map((d: ModelSpec) => {
-                    d.features = d.features.concat(
-                        d.targets ? [createTargetAsFeature(d.targets)] : []
-                    )
-                    return d
-                })
-
-                setIsLoadingModels(false)
-                setModels(models)
-            })
-            .catch((error: any) => {
-                //route probarly not set up, warning should be shown
-                setModels([offlineModel])
-                setSelectedModel(offlineModel)
-                setIsLoadingModels(false)
-            })
-    }
-
-    const onChangeModel = (event: any) => {
-        const selected = models?.find(
-            (d: ModelSpec) => d.name === event.selected
-        ) as ModelSpec
-        setSelectedModel(selected)
-    }
-
-    useEffect(() => {
-        getModels()
-    }, [])
-
-    return (
-        <div>
-            <SingleSelectField
-                tabIndex="1"
-                label={i18n.t('Select a model')}
-                loading={isLoadingModels}
-                placeholder="Select the model to use in prediction"
-                onChange={onChangeModel}
-                selected={selectedModel?.name}
-            >
-                {models?.map((d: ModelSpec) => (
-                    <SingleSelectOption
-                        key={d.name}
-                        value={d.name}
-                        label={d.name}
-                    />
-                ))}
-            </SingleSelectField>
-        </div>
-    )
-}
-
-const SelectModel: React.FC<SelectModelProps> = ({ selectedModel, setSelectedModel }) => {
+const SelectModel = ({ selectedModel, setSelectedModel }: SelectModelProps) => {
     const [models, setModels] = useState<ModelSpec[]>([]);
     const [isLoading, setIsLoading] = useState(true);
     const [showGrid, setShowGrid] = useState(true); // Control grid visibility
@@ -165,6 +106,9 @@ const SelectModel: React.FC<SelectModelProps> = ({ selectedModel, setSelectedMod
                             {/* Model Info */}
                             <h3 className={styles.modelHumanName}>{selectedModel.description}</h3>
                             <div className={styles.modelAuthor}>
+                                <span>
+                                    Author: 
+                                </span>
                                 <img src={selectedModel.logo || "/public/default-model-logo.png"} alt={selectedModel.name + " logo"} className={styles.modelAuthorLogo} />
                                 <span className={styles.modelAuthorName}>
                                     {selectedModel.author}
@@ -179,16 +123,20 @@ const SelectModel: React.FC<SelectModelProps> = ({ selectedModel, setSelectedMod
                                 ))}
                             </ul>
 
-                            <a href={selectedModel.link || ""} target="_blank">View model code</a>
+                            <p className={styles.modelLink}>
+                                External link: <a href={selectedModel.link || ""} target="_blank">{selectedModel.link || "http://example.com"}</a>
+                            </p>
 
                             {/* Show change button only if not showing grid */}
                             {!showGrid && (
-                                <button
-                                    className={styles.selectButton}
+                                <div>
+                                <Button
                                     onClick={() => setShowGrid(true)}
+                                    primary
                                 >
                                     Change Model
-                                </button>
+                                </Button>
+                                </div>
                             )}
                         </div>
                     )}
@@ -219,12 +167,12 @@ const SelectModel: React.FC<SelectModelProps> = ({ selectedModel, setSelectedMod
                                     </ul>
 
                                     {/* Select Button */}
-                                    <button
-                                        className={styles.selectButton}
+                                    <Button
                                         onClick={() => onChangeModel(model)}
+                                        primary
                                     >
                                         {selectedModel?.name === model.name ? "Selected" : "Select Model"}
-                                    </button>
+                                    </Button>
                                 </div>
                             ))}
                         </div>
