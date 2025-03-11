3516d863f95441c065184ce4af25f8eda3f02774
Herman Tretteteig <hermat@uio.no>
2025-03-11 16:08:43 +0100

    Edits for Karim

--- packages/prediction-app/src/features/prediction-results/ImportPrediction/ImportPrediction.tsx
+++ packages/prediction-app/src/features/prediction-results/ImportPrediction/ImportPrediction.tsx
@@ -1,9 +1,11 @@
 
 
-import React from 'react'
-import PredictionResult from '../../../components/results/PredictionResult'
+import React, { useEffect } from 'react'
+
 import { Result } from '../PredictionResults'
 import { Modal } from '@dhis2/ui'
+import PredictionResult from '../../../components/results/PredictionResult'
+
 
 interface ImportPredictionProps {
   setIsImportModalOpen: (value: boolean) => void
@@ -11,12 +13,30 @@ interface ImportPredictionProps {
   predictionToImport: Result
 }
 
-const ImportPrediction = ({isImportModalOpen, setIsImportModalOpen} : ImportPredictionProps) => {
+const ImportPrediction = ({isImportModalOpen, setIsImportModalOpen, predictionToImport} : ImportPredictionProps) => {
+
+  //fetch all prediction values
+  const fetchPredictionSamples = async (predictionId: string) => {
+
+  }
+
+  useEffect(() => {
+    fetchPredictionSamples(predictionToImport?.id)
+  }, [])
+  
+
   return (
+
+    
     <div>
       {isImportModalOpen &&
         <Modal onClose={() => {setIsImportModalOpen(false)}}>
           <div>
+            {/*show spinner while loading*/}
+
+            {/*when loaded show results and view to import*/}
+
+            {/*make a copy of this file in folder features/import-prediction*/}
             <PredictionResult />
           </div>
         </Modal>
