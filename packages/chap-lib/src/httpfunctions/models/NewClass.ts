35c911ad682cc157a577d2c7001a7b8b587e713c
eirikhaugstulen <eirik.haugstulen@gmail.com>
2025-05-14 18:53:18 +0200

    feat: add new types and update model definitions

--- packages/chap-lib/src/httpfunctions/models/NewClass.ts
+++ packages/chap-lib/src/httpfunctions/models/NewClass.ts
@@ -2,7 +2,7 @@
 /* istanbul ignore file */
 /* tslint:disable */
 /* eslint-disable */
-export type PredictionInfo = {
+export type NewClass = {
     datasetId: number;
     modelId: string;
     nPeriods: number;
