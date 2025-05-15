794246bea83c1df0bb9644cee2d5a0fff91ff5ae
Birk Johansson <birk@dhis2.org>
2025-05-15 15:06:41 +0200

    fix: working mvp

--- packages/prediction-app/src/features/select-evaluation/EvaluationSelector.tsx
+++ packages/prediction-app/src/features/select-evaluation/EvaluationSelector.tsx
@@ -1,21 +1,27 @@
-
-
 import React from 'react'
 import { SingleSelect, SingleSelectOption } from '@dhis2/ui'
 import { useQuery } from '@tanstack/react-query'
 import { BackTestRead, CrudService } from '@dhis2-chap/chap-lib'
+import css from './EvaluationSelector.module.css'
 
 interface EvaluationSelectorProps {
     onSelect: (evaluation: BackTestRead | undefined) => void
     selected?: BackTestRead | undefined
 }
 
-const EvaluationSelector: React.FC<EvaluationSelectorProps> = ({ onSelect, selected}) => {
-    const { data: evaluations, isLoading, isError, error } = useQuery({
+const EvaluationSelector: React.FC<EvaluationSelectorProps> = ({
+    onSelect,
+    selected,
+}) => {
+    const {
+        data: evaluations,
+        isLoading,
+        isError,
+        error,
+    } = useQuery({
         queryKey: ['evaluations'],
         queryFn: CrudService.getBacktestsCrudBacktestsGet,
-        cacheTime: 60 * 1000, // Cache for 60 seconds
-        staleTime: 60 * 1000, // Data is considered fresh for 60 seconds
+        staleTime: 60 * 5 * 1000, // Data is considered fresh for 60 seconds
     })
 
     if (isError) {
@@ -24,27 +30,34 @@ const EvaluationSelector: React.FC<EvaluationSelectorProps> = ({ onSelect, selec
     }
 
     return (
-        <SingleSelect
-
-            prefix='Evaluation'
-            placeholder="Select Evaluation"
-            onChange={({ selected }) => onSelect(evaluations?.find((evaluation) => evaluation.id.toString() === selected))}
-            selected={selected?.id.toString() || ''}
-            loading={isLoading}
-            error={isError}
-            clearable
-            filterable
-
-            
-        >
-            {evaluations?.map((evaluation) => (
-                <SingleSelectOption
-                    key={evaluation.id}
-                    value={evaluation.id.toString()}
-                    label={evaluation.name ?? ''}
-                />
-            ))}
-        </SingleSelect>
+        <div className={css.selectorWrapper}>
+            <SingleSelect
+                className={css.selector}
+                prefix="Evaluation"
+                placeholder="Select Evaluation"
+                onChange={({ selected }) =>
+                    onSelect(
+                        evaluations?.find(
+                            (evaluation) =>
+                                evaluation.id.toString() === selected
+                        )
+                    )
+                }
+                selected={selected?.id.toString() || ''}
+                loading={isLoading}
+                error={isError}
+                clearable
+                filterable
+            >
+                {evaluations?.map((evaluation) => (
+                    <SingleSelectOption
+                        key={evaluation.id}
+                        value={evaluation.id.toString()}
+                        label={evaluation.name ?? ''}
+                    />
+                ))}
+            </SingleSelect>
+        </div>
     )
 }
 
