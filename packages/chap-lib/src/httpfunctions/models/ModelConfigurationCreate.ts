27005efb10967956b723fd00588265768e1b5cbf
eirikhaugstulen <eirik.haugstulen@gmail.com>
2025-05-30 01:42:18 +0200

    chore: types

---
+++ packages/chap-lib/src/httpfunctions/models/ModelConfigurationCreate.ts
@@ -0,0 +1,11 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type ModelConfigurationCreate = {
+    name: string;
+    modelTemplateId: number;
+    userOptionValues?: (Record<string, any> | null);
+    additionalContinuousCovariates?: Array<string>;
+};
+
