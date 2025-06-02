8398a00aa41425a68fa2cd85d4c91f7932e2ca25
Birk Johansson <birk@dhis2.org>
2025-06-02 17:35:40 +0200

    fix(chart): optimize comparison by enabling virtuoso correctly

--- packages/chap-lib/src/components/evaluation/ComparisonPlotList/ComparisonPlotList.tsx
+++ packages/chap-lib/src/components/evaluation/ComparisonPlotList/ComparisonPlotList.tsx
@@ -8,7 +8,10 @@ interface ComparisonPlotListProps {
     useVirtuoso?: boolean
     useVirtuosoWindowScroll?: boolean
     virtuosoProps?: VirtuosoProps<any, any>
+<<<<<<< HEAD
     nameLabel?: string
+=======
+>>>>>>> 83c4d01 (fix(chart): optimize comparison by enabling virtuoso correctly)
 }
 
 export const ComparisonPlotList: React.FC<ComparisonPlotListProps> = ({
