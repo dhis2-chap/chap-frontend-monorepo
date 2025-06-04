d3376c0e7fcb9ef9786fed76a89cdf6c62a9c31b
Birk Johansson <birk@dhis2.org>
2025-06-04 12:44:23 +0200

    fix: fix d2-i18n types

--- packages/prediction-app/src/features/evaluation-compare/EvaluationCompare.tsx
+++ packages/prediction-app/src/features/evaluation-compare/EvaluationCompare.tsx
@@ -66,14 +66,8 @@ export const EvaluationCompare = () => {
                 v.evaluation.map((e) => ({
                     ...e,
                     orgUnitName:
-<<<<<<< HEAD
                         orgUnits?.find((ou) => ou.id === e.orgUnitId)
                             ?.displayName ?? e.orgUnitId,
-=======
-                        orgUnitsData?.organisationUnits?.find(
-                            (ou) => ou.id === e.orgUnitId
-                        )?.displayName ?? e.orgUnitId,
->>>>>>> dc1b5b8 (fix(chart): change chartlabel to evaluation)
                 }))
             )
             .sort((a, b) => a.orgUnitName.localeCompare(b.orgUnitName))
