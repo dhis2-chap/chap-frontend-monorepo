ca8e84952188891ccd66044c5da6ca03651219bf
eirikhaugstulen <eirik.haugstulen@gmail.com>
2025-06-05 01:28:29 +0200

    feat: add copy functionality for backtests with a new modal

--- packages/prediction-app/src/pages/NewEvaluationPage/NewEvaluationPage.tsx
+++ packages/prediction-app/src/pages/NewEvaluationPage/NewEvaluationPage.tsx
@@ -1,12 +1,9 @@
-import React, { useMemo } from 'react'
+import React from 'react'
 import i18n from '@dhis2/d2-i18n'
-import { useLocation } from 'react-router-dom'
 import { NewEvaluationForm } from '../../components/NewEvaluationForm'
 import { PageHeader } from '../../features/common-features/PageHeader/PageHeader'
-import { useOrgUnitsById } from '../../hooks/useOrgUnitsById'
-import { EvaluationFormValues } from '../../components/NewEvaluationForm'
-import { PERIOD_TYPES } from '../../components/NewEvaluationForm/Sections/PeriodSelector'
 import { CircularLoader } from '@dhis2/ui'
+import { useEvaluationFormState } from './hooks/useEvaluationFormState'
 
 export type EvaluationFormLocationState = {
   name?: string
@@ -30,41 +27,14 @@ export type EvaluationFormLocationState = {
  *     periodType: 'MONTH',
  *     fromDate: '2024-01',
  *     toDate: '2024-12',
- *     modelId: '123',
- *     orgUnits: ['org1', 'org2']
+ *     modelId: '5',
+ *     orgUnits: ['orgUnitId1', 'orgUnitId2']
  *   }
  * })
  * ```
  */
 export const NewEvaluationPage = () => {
-  const location = useLocation()
-  const locationState = location.state as EvaluationFormLocationState | undefined
-
-  const { data: orgUnitsData, isLoading: isOrgUnitsLoading } = useOrgUnitsById(locationState?.orgUnits || [])
-
-  const initialOrgUnits = useMemo(() => {
-    if (!locationState?.orgUnits?.length || !orgUnitsData?.organisationUnits) {
-      return []
-    }
-
-    return orgUnitsData.organisationUnits.map(ou => ({
-      id: ou.id,
-      displayName: ou.displayName,
-      name: ou.displayName,
-      path: ou.path,
-    }))
-  }, [locationState?.orgUnits, orgUnitsData])
-
-  const initialValues: Partial<EvaluationFormValues> = useMemo(() => ({
-    name: locationState?.name || '',
-    periodType: locationState?.periodType || PERIOD_TYPES.MONTH,
-    fromDate: locationState?.fromDate || '',
-    toDate: locationState?.toDate || '',
-    orgUnits: initialOrgUnits,
-    modelId: locationState?.modelId || '',
-  }), [locationState, initialOrgUnits])
-
-  const isLoading = isOrgUnitsLoading && locationState?.orgUnits && locationState.orgUnits.length > 0
+  const { initialValues, isLoading } = useEvaluationFormState()
 
   return (
     <div>
